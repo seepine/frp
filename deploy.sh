129c8c527036454aafa4a8454b2377f7c3f315ae
seepine <seepine@outlook.com>
2023-09-27 11:15:41 +0800

    chore(release): v0.51.3

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-docker buildx build -t seepine/frpc:v0.44.0 \
-   --platform=linux/amd64,linux/arm64 \
-   -f ./Dockerfile-frpc . --push
-
-docker buildx build -t seepine/frps:v0.44.0 \
-   --platform=linux/amd64,linux/arm64 \
-   -f ./Dockerfile-frps . --push
